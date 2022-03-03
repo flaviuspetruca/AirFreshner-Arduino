6040d89fa728d2b6635d930cb5312b1e442cc2f9
flaviuspetruca <flavius.petruca@stud.acs.upb.ro>
2022-03-03 20:23:23 +0100

    Working toilet numbers

--- ToiletFreshner.ino
+++ ToiletFreshner.ino
@@ -7,7 +7,7 @@
 //Distance sensor
 #define TRIGGER_PIN  9 // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     A0 // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 500 // Maximum distance we want to ping for (in centimeters).
+#define MAX_DISTANCE 100 // Maximum distance we want to ping for (in centimeters).
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
@@ -22,7 +22,7 @@ DallasTemperature sensors(&oneWire);
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 6, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // constants won't change.
@@ -30,7 +30,7 @@ const int LDR = A1;             // the pin for light sensor
 const int motionSensor = A2;    // the number of the motion sensor pin
 const int buttonsPin = A4;      // the number of the pushbutton pin
 const int greenLedPin = A5;     // the number of the green LED pin -- spraying
-const int freshner = 6;         // the number of the freshner pin
+const int freshner = 3;         // the number of the freshner pin
 const int redLedPin = 13;       // the number of the red LED pin -- system on
 const int magneticSensor = 8;   // the number of the magnetic sensor pin
 int state_magnetic;             // 0 close - 1 open wwitch
@@ -38,6 +38,9 @@ int state_magnetic;             // 0 close - 1 open wwitch
 //EEPROM cannot hold a value bigger then 255 in one byte so we will need two bytes in order to perform this;
 const int addressFirst2 = 0;       // memory address for the number of sprays
 const int addressLast2 = 1;        // memory address for the number of sprays
+
+//EEPROM for delay
+const int addressDelay = 2;        // memory address for delay variable
 
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
@@ -48,28 +51,85 @@ int distance = 0;         //distance sensor variable
 int light = 0;            //light sensor variable
 int motion;               //motion sensor variable
 int motionState = LOW;    // by default, no motion detected
+int numberOfSprays = 0;
+int lastSprayTime = 0;
+int betweenSpraysInterval = 30000;
+
+
+//debouncing
+int lastButtonState;
+unsigned long debounceDelay = 100;
+unsigned long lastDebounceTimeButton = 0;
+
+// in order to delay the spray have to keep track of the time the action to spray was initiated and if it has been sprayed or not
+int delayedSpray = 0;
+unsigned long initiatedSprayTime = 0;
 
 //menu variables
 int menuState = 0;        //inactive menu
 int selectedOption = -1;  // no selected option
 int currentOption = 0;    //variable to cycle through options
+volatile int greenLEDState = LOW;
 String options[] = { 
   "Spray Delay",
   "Replace can"
 };
-int optionValues[] = {15,1};
+int optionValues[] = {EEPROM.read(addressDelay),1};
 int optionLength = 1;
 
 
 //nonblocking delay variables
 unsigned long currentMillis;
-unsigned long previousMillisButtonSpray = 0;
+unsigned long previousMillisSpray = 0;
 unsigned long previousMillisSensors = 0;
 unsigned long previousMillisMenu = 0;
 int refreshInterval = 2000;
-int buttonInterval = 16000;
+int minimumInterval = 16000;
 int menuInterval = 5000;
 
+
+//variables required for in use checking and calculating the spray state
+const unsigned long nr2Time = 100000;
+const int nr1Time = 40000;
+const int distanceIntervalMin = 0;
+const int distanceIntervalMax = 40; 
+bool inInterval = false;
+//in order to detect the use of the toilet we check if the distance for the last 10 sensor data recieved are in the interval of distances in which the toilet might be in use
+int distancesArray[10];
+int distArrLength = 10;
+int inUseStart = -1;
+int inUseEnd = -1;
+int previnUseStart = -1;
+int previnUseEnd = -1;
+int useState = 0; //can be // -- 0 for not in use // -- 1 for number 1// --2 for number 2// -- (-1) for unknown // -- 3 for cleaning 
+
+void addDistanceToArray(){
+  int i;
+  inInterval = true;
+  for(i = 0; i < distArrLength - 1 ; i++){
+    distancesArray[i] = distancesArray[i + 1];
+    if(!(distancesArray[i] >= distanceIntervalMin) || !(distancesArray[i] <= distanceIntervalMax)){
+      inInterval = false;
+    }
+  }
+  distancesArray[i] = distance;
+  if(!(distancesArray[i] >= distanceIntervalMin) || !(distancesArray[i] <= distanceIntervalMax)){
+    inInterval = false;
+  }
+}
+
+void inUse(){
+  if(inInterval){
+    useState = -1;
+    inUseStart = millis();
+  } else {
+    if(inUseStart != -1){
+      inUseEnd = millis();
+    }
+  }
+}
+
+// number of sprays manipulation and initialization of EEPROM memory
 void initializeEEPROM(){
   EEPROM.update(addressFirst2, 24);
   EEPROM.update(addressLast2, 0);
@@ -106,6 +166,8 @@ void decreseNumberOfSpraysAndPrint(){
   displayNumberSprays();
 }
 
+// -------------DISPLAY AND MENU---------------------
+
 void initializeLCD(){
   // set up the LCD's number of columns and rows and print the available sprays:
   lcd.begin(16, 2);
@@ -121,7 +183,101 @@ void displayNumberSprays(){
   }
 }
 
+void displayTemperature(){
+  // set the cursor to column 10, line 1
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempCByIndex(0);
+  lcd.setCursor(10, 1);
 
+  //make only one decimal point string from temperature
+  String tempCString = String(tempC);
+  int length = tempCString.length();
+  tempCString.remove(length-1);
+  if (tempC != DEVICE_DISCONNECTED_C) {
+    lcd.print(tempCString + " C");
+  }
+}
+
+void displayMenu(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(F("Options"));
+  lcd.setCursor(0,1);
+  lcd.print("< " + options[currentOption] + " >");
+}
+
+void displayMainScreen(){
+  displayNumberSprays();
+  displayTemperature();
+}
+
+void displayCurrentOption(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(options[selectedOption]);
+  displayOptionValue();
+}
+
+void displayOptionValue(){
+  lcd.setCursor(7,1);
+  if(selectedOption == 0){
+    lcd.print(String(optionValues[selectedOption]) + " s");
+  } else {
+    lcd.print("Yes ->");
+  }
+}
+
+void resetMenu(){
+  if(currentMillis - previousMillisMenu >= menuInterval && menuState){
+    menuState = 0;
+    currentOption = 0;
+    selectedOption = -1;
+    lcd.clear();
+    displayMainScreen();
+  }
+}
+
+void increaseOptionValue(){
+  if(selectedOption == 0){
+    if(optionValues[selectedOption] < 60){
+      optionValues[selectedOption]++;
+      EEPROM.update(addressDelay, optionValues[selectedOption]);
+    }
+  } else {
+    //replaceCan
+    initializeEEPROM();
+    numberOfAvailableSprays = 2400;
+    lcd.clear();
+    displayMainScreen();
+  }
+}
+
+void decreaseOptionValue(){
+  if(selectedOption == 0){
+    if(optionValues[selectedOption] > 15){
+      optionValues[selectedOption]--;
+      EEPROM.update(addressDelay, optionValues[selectedOption]);
+    }
+  } 
+}
+
+void scrollOptionRight(){
+  if(currentOption < optionLength){
+    currentOption++;
+    displayMenu();
+    delay(500);
+  }
+}
+
+void scrollOptionLeft(){
+  if(currentOption > 0){
+    currentOption--;
+    displayMenu();
+    delay(500);
+  }
+}
+
+// -------------SETUP---------------------
 //initialize pins for leds
 void setupLEDS(){
   pinMode(greenLedPin, OUTPUT);
@@ -160,42 +316,25 @@ void setupButtons(){
 }
 
 void setup() {
-  //this functions is run only once at the first setup of the whole system
+  attachInterrupt(1, changeGreenLedState, CHANGE);
   setupMagnetic();
   setupLEDS();
   setupTemperatureSensor();
-  //initializeEEPROM();
   initializeLCD();
   setupFreshner();
   setupButtons();
+  Serial.begin(9600);
 }
 
-
-void displayTemperature(){
-  // set the cursor to column 10, line 1
-  sensors.requestTemperatures();
-  float tempC = sensors.getTempCByIndex(0);
-  lcd.setCursor(10, 1);
-
-  //make only one decimal point string from temperature
-  String tempCString = String(tempC);
-  int length = tempCString.length();
-  tempCString.remove(length-1);
-  if (tempC != DEVICE_DISCONNECTED_C) {
-    lcd.print(tempCString + " C");
-  }
-}
-
+// -------------GET VALUES SENSORS AND ACTUATORS---------------------
 void getMotion(){
   motion = digitalRead(motionSensor);   // read sensor value
   if (motion == HIGH) {              // check if the sensor is HIGH
-    delay(200);
     if (motionState == LOW) {
       motionState = HIGH;         // update variable state to HIGH
     }
   } 
   else {
-    delay(200);
     if (motionState == HIGH){
         motionState = LOW;        // update variable state to LOW
     }
@@ -210,164 +349,164 @@ void getLight(){
   light = analogRead(LDR);
 }
 
-void resetMenu(){
-  if(currentMillis - previousMillisMenu >= menuInterval && menuState){
-    menuState = 0;
-    currentOption = 0;
-    selectedOption = -1;
-    lcd.clear();
-    displayMainScreen();
+void getFlushState(){
+  //magnetic sensor state
+  state_magnetic = digitalRead(magneticSensor);
+}
+
+bool buttonStateChanged(){
+  if(lastButtonState > 900 && buttonState > 900){
+    return false;
+  } else if((lastButtonState > 300 && lastButtonState < 900) && (buttonState > 300 && buttonState < 900)){
+    return false;
+  } else if((lastButtonState > 50 && lastButtonState < 300) && (buttonState > 50 && buttonState < 300)){
+    return false;
   }
-}
-void displayMenu(){
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(F("Options"));
-  lcd.setCursor(0,1);
-  lcd.print("< " + options[currentOption] + " >");
+  return true;
 }
 
-void displayMainScreen(){
-  displayNumberSprays();
-  displayTemperature();
-}
-
-void displayCurrentOption(){
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(options[selectedOption]);
-  displayOptionValue();
-}
-
-void displayOptionValue(){
-  lcd.setCursor(7,1);
-  if(selectedOption == 0){
-    lcd.print(String(optionValues[selectedOption]) + " s");
-  } else {
-    lcd.print("Yes ->");
-  }
-}
-
-void increaseOptionValue(){
-  if(selectedOption == 0){
-    if(optionValues[selectedOption] < 60){
-      optionValues[selectedOption]++;
+// -------------SPRAY EVENTS---------------------
+void calculateNumberOfSprays(){
+  if(state_magnetic == HIGH){
+    if(inUseEnd != -1 && inUseStart != -1 && inUseEnd > inUseStart && previnUseEnd != inUseEnd && previnUseStart != inUseStart){
+      int timeOnToilet = inUseEnd - inUseStart;
+      previnUseStart = inUseStart;
+      previnUseEnd = inUseEnd;
+      if(timeOnToilet > nr2Time){
+        useState = 2;
+        numberOfSprays += 2;
+      } else if(timeOnToilet > nr1Time && timeOnToilet < nr2Time){
+        useState = 1;
+        numberOfSprays++;
+      } else {
+        useState = 3;
+      }
+    } else {
+      useState = 3;
     }
   } else {
-    //replaceCan
-    initializeEEPROM();
-    numberOfAvailableSprays = 2400;
-    lcd.clear();
-    displayMainScreen();
-  }
-}
-
-void decreaseOptionValue(){
-  if(selectedOption == 0){
-    if(optionValues[selectedOption] > 15){
-      optionValues[selectedOption]--;
-    }
-  } 
-}
-
-void scrollOptionRight(){
-  if(currentOption < optionLength){
-    currentOption++;
-    displayMenu();
-    delay(500);
-  }
-}
-
-void scrollOptionLeft(){
-  if(currentOption > 0){
-    currentOption--;
-    displayMenu();
-    delay(500);
+    useState = 0;
   }
 }
 
 void checkPossibleSpray(){
-  if(distance < 10 && state_magnetic == HIGH){
-    if(millis() - previousMillisButtonSpray >= buttonInterval){
-      spray();
-    }
+  if(numberOfSprays && millis()-lastSprayTime >= betweenSpraysInterval){
+    numberOfSprays--;
+    spray();
+    lastSprayTime = millis();
   }
 }
 
 void spray(){
-  analogWrite(freshner, 255);
-  digitalWrite(greenLedPin, HIGH);
-  previousMillisButtonSpray = millis();
-  decreseNumberOfSpraysAndPrint();
+  initiatedSprayTime = millis();
+  if(optionValues[0] > 16){
+    delayedSpray = 1;
+  } else if(currentMillis - previousMillisSpray >= minimumInterval){
+    analogWrite(freshner, 255);
+    previousMillisSpray = millis();
+    decreseNumberOfSpraysAndPrint();
+  }
+}
+
+void delaySpray(){
+  if(delayedSpray == 1 && optionValues[0] > 16 && (millis() - initiatedSprayTime >= optionValues[0]*1000 - minimumInterval)){
+    analogWrite(freshner, 255);
+    previousMillisSpray = millis();
+    decreseNumberOfSpraysAndPrint();
+    delayedSpray = 0;
+  }
+}
+
+void changeGreenLedState(){
+    greenLEDState = !greenLEDState;
 }
 
 void loop() { 
-  // read the state of the pushbutton value:
+  //check if toilet is in use
+  inUse();
+  //checks if there is a spray required and performs the action
   checkPossibleSpray();
+  //chaning greenLedState based on the interrupt
+  digitalWrite(greenLedPin, greenLEDState);
+  //if the selay has been configured to another time
+  delaySpray();
+  //5 seconds wait time for the menu to dissapear
+  resetMenu();
+  // read the state of the pushbutton value:
   buttonState = analogRead(buttonsPin);
+  if(buttonStateChanged()){
+    lastDebounceTimeButton = millis();
+  }
+  
   currentMillis = millis();
   if(currentMillis - previousMillisSensors >= refreshInterval && !menuState){
     displayMainScreen();
     getLight();
-    getDistance();
-    getMotion();
+    //checking for light in order not to ask for data when not needed
+    //experiment done in windowless bathroom 
+    if(light > 100){
+      getFlushState();
+      getDistance();
+      addDistanceToArray();
+      getMotion();
+      calculateNumberOfSprays();
+      Serial.println(String(useState) + '\n' + "DISTANCE: " + String(distance));
+    }
     previousMillisSensors = currentMillis; 
   }
-  
-  //5 seconds wait time for the menu to dissapear
-  resetMenu();
-  
-  //magnetic sensor state
-  state_magnetic = digitalRead(magneticSensor);
-  
+
+
+  //Menu And Button Spray
   currentMillis = millis();
-  if(buttonState < 50){
-    if(currentMillis - previousMillisButtonSpray >= buttonInterval){
+  if(buttonState < 50){ //no button press
+    if(currentMillis - previousMillisSpray >= optionValues[0]*1000){
       analogWrite(freshner, 0);
-      digitalWrite(greenLedPin, LOW);
     }
-  } else if(buttonState > 900){
-    if(menuState){
-      if(selectedOption != -1){
-        increaseOptionValue();
-        displayOptionValue();
-        delay(500);
-      } else {
-        scrollOptionRight();
+  }
+  if ((currentMillis - lastDebounceTimeButton) > debounceDelay){
+    if(buttonState > 900){ // right most button press
+      if(menuState){
+        if(selectedOption != -1){
+          increaseOptionValue();
+          displayOptionValue();
+          delay(500);
+        } else {
+          scrollOptionRight();
+        }
       }
-    }
-    previousMillisMenu = currentMillis;
-  } else if(buttonState > 300){
-    if(menuState){
-      if(selectedOption == -1){
-        selectedOption = currentOption;
-        displayCurrentOption();
-        delay(200);
+      previousMillisMenu = currentMillis;
+    } else if(buttonState > 300){ // middle button press
+      if(menuState){
+        if(selectedOption == -1){
+          selectedOption = currentOption;
+          displayCurrentOption();
+          delay(200);
+        } else {
+          //go back
+          selectedOption = -1;
+          displayMenu();
+          delay(200);
+        }
       } else {
-        //go back
-        selectedOption = -1;
+        menuState = 1;
         displayMenu();
         delay(200);
       }
-    } else {
-      menuState = 1;
-      displayMenu();
-      delay(500);
+      previousMillisMenu = currentMillis;
+    } else if(buttonState > 50){ // left most button press
+        if(!menuState){
+          numberOfSprays++;
+        } else {
+            if(selectedOption != -1){
+              decreaseOptionValue();
+              displayOptionValue();
+              delay(500);
+            } else {
+              scrollOptionLeft();
+            }
+            previousMillisMenu = currentMillis;
+        }
     }
-    previousMillisMenu = currentMillis;
-  } else if(buttonState > 50){
-      if(!menuState){
-        //spray
-        spray();
-      } else {
-          if(selectedOption != -1){
-            decreaseOptionValue();
-            displayOptionValue();
-            delay(500);
-          } else {
-            scrollOptionLeft();
-          }
-          previousMillisMenu = currentMillis;
-      }
   }
+  lastButtonState = buttonState;
 }
